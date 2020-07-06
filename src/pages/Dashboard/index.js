286a2e0530960765b1deb7b612ced785d43c68a1
Olivia Wall <theoliviakatelyn@gmail.com>
2020-07-05 19:00:06 -0500

    Video on Employer Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './styles.scss';
 
 // import QRcodes from '../../components/QRCodes';
-// import Lesson from '../../components/lesson';
-// import Videos from '../../components/Videos';
+import Lesson from '../../components/lesson';
+import Videos from '../../components/Videos';
 
 const Dashboard = props => {
     return (
+        <div className='dash'>
         <h1>
             Welcome to your dashboard!
             {/* <QRcodes /> */}
-            {/* <Lesson /> */}    
-            {/* <Videos />        */}
+            <Lesson />    
+            <Videos />       
         </h1> 
+        </div>
     );
 };
 
