31516b7532c60f59362b130043be3606853fb99e
Olivia Wall <theoliviakatelyn@gmail.com>
2020-07-06 20:09:02 -0500

    Latest Updates with Video player

---
+++ src/pages/BusinessDashboard/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import './styles.scss';
+
+// // import QRcodes from '../../components/QRCodes';
+// // import Lesson from '../../components/lesson';
+// // import Videos from '../../components/Videos';
+
+// const BusinessDashboard = props => {
+//     return (
+//         <div className='dash'>
+//         <h1>
+//             Welcome to your dashboard!
+//             {/* <QRcodes /> */}
+//             {/* <Lesson />     */}
+//             {/* <Videos />        */}
+//         </h1> 
+//         </div>
+//     );
+// };
+
+// export default BusinessDashboard;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {Link} from 'react';
 import './styles.scss';
+import VideoGrid from './../VideoGrid';
+// import Button from './../../../../
+
 
 // import QRcodes from '../../components/QRCodes';
-import Lesson from '../../components/lesson';
-import Videos from '../../components/Videos';
+// import Lesson from '../../components/lesson';
+// import Videos from '../../components/Videos';
 
 const Dashboard = props => {
     return (
         <div className='dash'>
-        <h1>
-            Welcome to your dashboard!
-            {/* <QRcodes /> */}
-            <Lesson />    
-            <Videos />       
-        </h1> 
+            <h1>
+                Welcome to your dashboard!
+            <ul>
+                    <li>
+                        <Link to='/videogrid'>
+                            Applicants Video Resumes
+                        </Link>
+                    </li>
+                </ul>
+          
+                    <VideoGrid />
+                
+                
+                {/* <QRcodes /> */}
+                {/* <Lesson />     */}
+                {/* <Videos />        */}
+            </h1>
         </div>
     );
 };
